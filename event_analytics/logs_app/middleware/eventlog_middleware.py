f4d25909c78424141008e148ef7f8fb965ae833f
krithikvaidya <krithikvaidya@gmail.com>
2020-02-17 23:35:58 +0530

    Improve logging, add IP based Geolocation

--- event_analytics/logs_app/middleware/eventlog_middleware.py
+++ event_analytics/logs_app/middleware/eventlog_middleware.py
@@ -1,6 +1,6 @@
 import json  # to convert our dict into the JSON format
 from django.utils import timezone  # to get the date of the request
-
+from simple_geoip import GeoIP
 
 class LogsAppMiddleware(object):
 
@@ -23,22 +23,43 @@ class LogsAppMiddleware(object):
 
         # get the client's IP address
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        event_log['ip_address'] = x_forwarded_for.split(
-            ',')[0] if x_forwarded_for else request.META.get('REMOTE_ADDR')
+
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[-1].strip()
+        elif request.META.get('HTTP_X_REAL_IP'):
+            ip = request.META.get('HTTP_X_REAL_IP')
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        
+        event_log['ip_address'] = ip
+
+        # get method of request (GET, POST, etc)
         event_log['method'] = request.method
 
         # get name of browser used to make the request
         event_log['browser_info'] = request.META['HTTP_USER_AGENT']
 
-        # TODO: get location based on IP address
-        event_log['location'] = ''
+        # get location based on ip
+        geoip = GeoIP("at_w3VVriiq1AGRdxPb0bYZIhhR8KoxL")
+        data = geoip.lookup(ip)
+        event_log['location'] = data['location']['city']
 
         # Timestamp of event
         event_log['timestamp'] = str(timezone.now())
 
         # TODO: store insensitive GET/POST request information
-        event_log['data'] = str(request)
+        headers = ''
+        for header, value in request.META.items():
+            if not header.startswith('HTTP'):
+                continue
+            header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+            headers += '{}: {}\n'.format(header, value)
 
+        meta = request.META['CONTENT_TYPE']
+        ct = request.META['CONTENT_TYPE']
+        # print(f'{request.method} HTTP/1.1\nContent-Length: {meta}\nContent-Type: {ct}\n{headers}\n\n{request.body}')
+       
+        # append the above log to the JSON file
         try:
             with open('logs.json', 'a', encoding='utf-8') as f:
                 json.dump(event_log, f, ensure_ascii=False, indent=4)
